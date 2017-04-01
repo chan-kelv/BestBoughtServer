7c16a8f86281554247bc2486b59d606743ecfcec
Ubuntu <chank89@BestBought.aibxoytzucgu1flfatnvy3f4sb.dx.internal.cloudapp.net>
2017-04-01 18:27:42 +0000

    comment sentiment structs built and saved

--- main.go
+++ main.go
@@ -83,12 +83,13 @@ func Dev(w http.ResponseWriter, req *http.Request) {
 		comments := getCommentsFromResp(respBodyBytes)
 
 		//json package for microsoft cog labs
-		microsoftJson := parseCommentsForMicrosoft(comments)
+		microsoftJson, nlpComment := parseCommentsForMicrosoft(comments)
 
 		//call sentiment analysis
-		microsoftSentiment(microsoftJson)
+		microsoftSentiment(microsoftJson, nlpComment)
+		// microsoftKey(microsoftJson)
 
-		// fmt.Println(string(microsoftJson))
+		fmt.Println(nlpComment)
 
 	}
 }
@@ -109,34 +110,71 @@ func getCommentsFromResp(respBodyBytes []byte) []string {
 			if k2 == "comment" {
 				comments = append(comments, v2.(string))
 			}
-			fmt.Println(k2, ":=:", v2)
+			// fmt.Println(k2, ":=:", v2)
 		}
-		fmt.Println("\n\n")
+		// fmt.Println("\n\n")
 	}
 	return comments
 }
 
-func parseCommentsForMicrosoft(comments []string) []byte {
+func parseCommentsForMicrosoft(comments []string) ([]byte, map[string]NlpComment) {
 	//TODO: stop if ID>100
 	idCount := 1
+	commentMap := make(map[string]NlpComment)
 	var d Document
 	for _, comment := range comments {
 		commElement := DocElement{Language: "en", Id: strconv.Itoa(idCount), Text: comment}
 		d.Documents = append(d.Documents, commElement)
+
+		commentMap[strconv.Itoa(idCount)] = NlpComment{CommentText: comment}
+
 		idCount++
 	}
 	j, err := json.Marshal(d)
 	if err != nil {
 		fmt.Println("json marshal error", err)
-		return nil
+		return nil, nil
 	}
-	return j
+	return j, commentMap
 }
 
-func microsoftSentiment(json []byte) {
+func microsoftSentiment(jsonByte []byte, nlpComm map[string]NlpComment) {
 	//
 	// )
 	url := "https://westus.api.cognitive.microsoft.com/text/analytics/v2.0/sentiment"
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonByte))
+	req.Header.Set("Ocp-Apim-Subscription-Key", "9122668a88454ac9bed0b816edbe5c8c")
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Accept", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	body, _ := ioutil.ReadAll(resp.Body)
+	// fmt.Println("response body", string(body))
+	var sentResp SentimentResponse
+	// var sentResp []interface{}
+	err = json.Unmarshal(body, &sentResp)
+	if err != nil {
+		fmt.Println("JSON unmarshall error:", err)
+		return
+	}
+	// fmt.Println("Sent response", sentResp.Documents)
+	for _, sent := range sentResp.Documents {
+		s := nlpComm[sent.Id]
+		s.SentimentScore = sent.Score
+		nlpComm[sent.Id] = s
+	}
+}
+
+func microsoftKey(json []byte) {
+
+	url := "https://westus.api.cognitive.microsoft.com/text/analytics/v2.0/keyPhrases"
 	req, err := http.NewRequest("POST", url, bytes.NewBuffer(json))
 	req.Header.Set("Ocp-Apim-Subscription-Key", "9122668a88454ac9bed0b816edbe5c8c")
 	req.Header.Set("Content-Type", "application/json")
@@ -151,7 +189,10 @@ func microsoftSentiment(json []byte) {
 	defer resp.Body.Close()
 
 	body, _ := ioutil.ReadAll(resp.Body)
-	fmt.Println("response body", string(body))
+	// var x map[string]interface{}
+	// json.Unmarshal(byte[](body), &x)
+	// fmt.Println("\n\n")
+	fmt.Println("keywords", string(body))
 }
 
 func httpGet(url string) (*http.Response, error) {
@@ -171,4 +212,19 @@ type DocElement struct {
 
 type Document struct {
 	Documents []DocElement `json:"documents"`
+}
+
+type Sentiment struct {
+	Score float64 `json:"score"`
+	Id    string  `json:"id"`
+}
+
+type SentimentResponse struct {
+	Documents []Sentiment `json:"documents"`
+	Errors    []string    `json:"errors"`
+}
+
+type NlpComment struct {
+	CommentText    string
+	SentimentScore float64
 }
