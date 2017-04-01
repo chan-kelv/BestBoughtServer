b8f510ca207b15749e285e7d8b3e383c79568b66
Ubuntu <chank89@BestBought.aibxoytzucgu1flfatnvy3f4sb.dx.internal.cloudapp.net>
2017-04-01 07:46:35 +0000

    can grab list of comments from hardcoded link

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"strconv"
@@ -15,13 +17,14 @@ func main() {
 	server.HandleFunc("/", Index).Methods("GET")
 	server.HandleFunc("/hello/{name}", Hello)
 	server.HandleFunc("/product/{prodId}", CommentNLP).Methods("GET")
+	server.HandleFunc("/dev/", Dev)
 	// http.Handle("/", server)
 	log.Fatal(http.ListenAndServe(":8080", server))
 }
 
 func Index(w http.ResponseWriter, req *http.Request) {
-	fmt.Println("Index path")
-	fmt.Fprintf(w, "Index")
+	fmt.Println("Hello there")
+	fmt.Fprintf(w, "Hello there!")
 }
 
 func Hello(w http.ResponseWriter, req *http.Request) {
@@ -47,4 +50,45 @@ func CommentNLP(w http.ResponseWriter, req *http.Request) {
 	}
 	fmt.Println("Product received", prodId)
 	fmt.Fprintf(w, "Product received %s", prodIdRaw)
+}
+
+func Dev(w http.ResponseWriter, req *http.Request) {
+	resp, err := httpGet("http://www.bestbuy.ca/api/v2/json/reviews/10415309?page=1&pagesize=20&source=us")
+	if err != nil {
+		fmt.Println("GET error:", err)
+		return
+	}
+	//all good in the hood
+	if resp.StatusCode >= 200 && resp.StatusCode <= 300 {
+		respBodyBytes, _ := ioutil.ReadAll(resp.Body)
+
+		var dat map[string]interface{}
+		if err := json.Unmarshal(respBodyBytes, &dat); err != nil {
+			panic(err)
+		}
+		var comments []string
+		for _, val := range dat["reviews"].([]interface{}) {
+			v := val.(map[string]interface{})
+			for k2, v2 := range v {
+				if k2 == "comment" {
+					comments = append(comments, v2.(string))
+				}
+				fmt.Println(k2, ":=:", v2)
+			}
+			fmt.Println("\n\n")
+		}
+
+		fmt.Println(comments)
+
+		// fmt.Fprintf(w, dat)
+	}
+
+}
+
+func httpGet(url string) (*http.Response, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, fmt.Errorf("GET error", err)
+	}
+	return resp, nil
 }
